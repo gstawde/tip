c90ebbd0669f80f9f2ddb17bd771eb4197f199b5
GargiTawde <gargi.tawde01@gmail.com>
2020-06-28 23:31:46 -0700

    Full functioning app: - calculates tip based on input in text field and selected tip amount - calculates total and updates value - added custom app icon art

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get Bill Amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+        // Calculate Tip and Total
+        let tipPercentages = [0.15,0.18,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        // Update Tip Amount and Total
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
+    }
+    
+    
 }
 
